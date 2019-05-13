6745def04130bd7e4380bf9f2a3be2966ef1d934
Cassandra Hoo <cahoo@uci.edu>
2019-05-12 17:16:16 -0700

    Added .listen(port);

---
+++ server.js
@@ -0,0 +1,28 @@
+/* Include the static file webserver library */
+var static = require ('node-static');
+
+/* Include the http server library */
+var http = require('http');
+
+/*Assume that we are running on Heroku */
+var port = process.env.PORT;
+var directory = __dirname + '/public';
+
+/* If we aren't on Heroku, then we need to readjust the port and directory information and we know that because port won't be set */
+if (typeof port == 'undefined' || !port){
+    directory = './public'o;
+    port = 8080;
+}
+
+/* Set up a static web-server that will deliver files from the file system */
+var file = new static.Server(directory);
+
+/* Construct an http server that gets files from the file server */
+var app = http.createServer(
+      funtion(request,response){
+          request.addListener('end',
+              function(){
+                  file.server(request,response);
+              }
+          ).resume();
+      }
